36d5697ce075e75f7c8b4dac4c874981ef06d504
Justin Obara <obarajustin@gmail.com>
2014-10-10 10:05:18 -0400

    GPII-947: Update copyrights

--- src/signup/js/signup.js
+++ src/signup/js/signup.js
@@ -2,7 +2,12 @@
  * Cloud4all Preferences Management Tools - Sign Up UI
  *
  * Copyright 2014 CERTH/HIT
+ * Copyright 2014 OCAD University
  *
+ * Licensed under the New BSD license. You may not use this file except in
+ * compliance with this License.
+ * You may obtain a copy of the License at
+ * https://github.com/GPII/prefsEditors/LICENSE.txt
  */
 
 var confirmPasswd;
